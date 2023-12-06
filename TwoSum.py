337794c291690e96a74cccf266aa096d81a1b5d7
JosephAFerguson <99895281+JosephAFerguson@users.noreply.github.com>
2023-12-06 16:06:56 -0500

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i in range(len(nums)):
-            for j in range(1,len(nums)):
-                if ((nums[i] + nums[j]) == target) and (i != j):
-                    return [i, j]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i,val in enumerate(nums):
+            h[val] = i
+        for i,val in enumerate(nums):
+            if h.get(target- val,-99)!=-99 and h.get(target- val,-99)!=i :
+                return [i,h[target- val]]
