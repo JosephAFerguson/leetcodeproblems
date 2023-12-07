9f40e4b4ea345329a4a091ca97951a41d70a7cf7
JosephAFerguson <99895281+JosephAFerguson@users.noreply.github.com>
2023-12-07 14:27:46 -0500

    Update CapitalizeTheTital.py

--- CapitalizeTheTital.py
+++ CapitalizeTheTital.py
@@ -1,30 +1,15 @@
-class Solution(object):
-    def capitalizeTitle(self, title):
-        """
-        :type title: str
-        :rtype: str
-        """
-        words = []
-        word = ""
-        finalword =""
-        for i in range(len(title)):
-            if title[i] != " ":
-                word += title[i]
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        title = title.split()
+        new = []
+        for i in title:
+            if len(i)<3:
+                i = i.lower()
+                new.append(i)
             else:
-                word = word.lower()
-                words.append(word)
-                word = ''
-        word = word.lower()
-        words.append(word)
-        for i in words:
-            firstletter = ""
-            newword = ""
-            if len(i) > 2:
-                firstletter += i[0]
-                firstletter = firstletter.upper()
-                newword = i[1:]
-                finalword += firstletter + newword + " "
-            else:
-                finalword += i + " "
-        finalword  = finalword[:-1]
-        return finalword
+                i = i.lower()
+                val = i[0]
+                val = val.upper()
+                i = val + i[1:]
+                new.append(i)
+        return ' '.join(new)
