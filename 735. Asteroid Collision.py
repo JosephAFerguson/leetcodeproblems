5269f5c75eae5a7168f8ca53e8d0c98e30437937
JosephAFerguson <99895281+JosephAFerguson@users.noreply.github.com>
2023-12-12 09:12:59 -0500

    Update 735. Asteroid Collision.py

--- 735. Asteroid Collision.py
+++ 735. Asteroid Collision.py
@@ -4,13 +4,12 @@ class Solution:
         for a in asteroids:
             stack.append(a)
             #compare last two asteroids
-            if len(stack) > 1:
-                while len(stack) > 1 and stack[-1]<0 and stack[-2] >0:
-                    if abs(stack[-1]) > abs(stack[-2]):
-                        stack.pop(-2)
-                    elif abs(stack[-1]) < abs(stack[-2]):
-                        stack.pop()
-                    else:
-                        stack.pop()
-                        stack.pop()
+            while len(stack) > 1 and stack[-1]<0 and stack[-2] >0:
+                if abs(stack[-1]) > abs(stack[-2]):
+                    stack.pop(-2)
+                elif abs(stack[-1]) < abs(stack[-2]):
+                    stack.pop()
+                else:
+                    stack.pop()
+                    stack.pop()
         return stack
