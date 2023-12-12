5b0aae81be897038913c8d3d18286f7e4efaae6e
JosephAFerguson <99895281+JosephAFerguson@users.noreply.github.com>
2023-12-12 09:11:15 -0500

    Create 735. Asteroid Collision.py

---
+++ 735. Asteroid Collision.py
@@ -0,0 +1,16 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            stack.append(a)
+            #compare last two asteroids
+            if len(stack) > 1:
+                while len(stack) > 1 and stack[-1]<0 and stack[-2] >0:
+                    if abs(stack[-1]) > abs(stack[-2]):
+                        stack.pop(-2)
+                    elif abs(stack[-1]) < abs(stack[-2]):
+                        stack.pop()
+                    else:
+                        stack.pop()
+                        stack.pop()
+        return stack
