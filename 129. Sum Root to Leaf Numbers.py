061e31219c5716084d24df18ee54ce05c89f8e48
JosephAFerguson <99895281+JosephAFerguson@users.noreply.github.com>
2024-04-17 12:36:49 -0400

    Update 129. Sum Root to Leaf Numbers.py

--- 129. Sum Root to Leaf Numbers.py
+++ 129. Sum Root to Leaf Numbers.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    paths  = []
+    r= 0
     def dfs(self, root, currP):
         #make sure we do not add any node paths that have children
         if not root:
@@ -14,11 +14,8 @@ class Solution:
         self.dfs(root.left, currP)
         self.dfs(root.right, currP)
         if not root.left and not root.right:
-            self.paths.append(currP)
+            self.r+=int(currP)
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        r = 0
-        self.paths = []
+        self.r = 0
         self.dfs(root,"")
-        for i, val in enumerate(self.paths):
-            r += int(val)
-        return r
+        return self.r
