c7280496caa26ec0fa7a6a0d15cfb6236ac25528
JosephAFerguson <99895281+JosephAFerguson@users.noreply.github.com>
2024-06-04 09:34:31 -0400

    Create 942. DI String Match.py

---
+++ 942. DI String Match.py
@@ -0,0 +1,22 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        n = len(s)
+        res = [0] * (n+1)
+        j = 0
+        big = n
+        small = 0
+        while j < n:
+            if s[j] == "D":
+                res[j] = big
+                big -=1
+            else:
+                res[j] = small
+                small +=1
+            j+=1
+        if s[j-1] == "D":
+            res[j] =(big)
+        else:
+            res[j] =(small)
+        return res
+
+
