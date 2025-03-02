e6aa9a92874ccdf63538d37d6ade9cea70f7438c
Joseph Ferguson <99895281+JosephAFerguson@users.noreply.github.com>
2025-03-02 15:36:02 -0500

    Delete Code Testcase Testcase Test Result 1823. Find the Winner of the Circular Game - Josephus solution.py

--- Code Testcase Testcase Test Result 1823. Find the Winner of the Circular Game - Josephus solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findTheWinner(self, n: int, k: int) -> int:
-        i = 1
-        ans = 0
-        while i <= n:
-            ans = (ans+k)%i
-            i+=1
-        return ans+1
